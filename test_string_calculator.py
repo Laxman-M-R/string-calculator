e1235f17fa32065f78f9d6d15ce0617f379ac98b
Laxman-M-R <laxman.rm098@gmail.com>
2022-05-21 23:17:31 +0530

    removed comments

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -14,15 +14,12 @@ def test_multiple_numbers():
     assert add("1,2,3,4") == 10
 
 def test_new_line_as_delimiter():
-    """it should be able to use new lines as delimiters"""
     assert string_calculator.add('1\n2,3') == 6
 
 def test_different_delimiters():
-    """it should be able handle different delimiters"""
     assert string_calculator.add('//;\n1;2') == 3
 
 def test_negative_numbers_exception():
-    """it should raise an exception containing all negative numbers if any are found"""
     with pytest.raises(Exception, match = r'negatives not allowed \[-1, -2, -3\]'):
         string_calculator.add('-1, -2, -3, 1, 2, 3')
 
