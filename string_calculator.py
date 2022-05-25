8317d7fd66cc2ce263efbe33156489ea7feab2c4
Laxman-M-R <laxman.rm098@gmail.com>
2022-05-25 19:43:35 +0530

    added add_diff method

--- string_calculator.py
+++ string_calculator.py
@@ -1,25 +1,50 @@
 import re
 
-def add(numbers):
+def valid_numbers(numbers):
     if numbers == '':
-        return 0
+        return [0], []
 
     numbers = list(map(int, re.findall(r"-?\d+", numbers)))
     negative_numbers = list(filter(lambda x: x < 0, numbers))
 
-
-    if negative_numbers:
-        raise Exception('negatives not allowed ' + str(negative_numbers))
-
-    return (sum(numbers))
+    return numbers, negative_numbers
 
 
-print(add(""))
-print(add("1,2"))
-print(add("1"))
-print(add("1,2,3,4"))
-print(add('1\n2,3'))
-print(add('//;\n1;2'))
-print(add('//[***]\n1***2***3'))
-print(add('//[*][%]\n1*2%3'))
-print(add('-1, -2, -3, 1, 2, 3'))
+def add(numbers):
+
+    numbers = valid_numbers(numbers)
+
+    if numbers[1]:
+        raise Exception('negatives not allowed ' + str(numbers[1]))
+
+    elif numbers[0]:
+        return (sum(numbers[0]))
+
+def add_diff(numbers):
+    numbers = valid_numbers(numbers)
+
+    if numbers[1]:
+        raise Exception('negatives not allowed ' + str(numbers[1]))
+
+    elif numbers[0]:
+        even_sum = 0
+        odd_sum = 0
+
+        for i, n in enumerate(numbers[0]):
+            if i % 2 == 0:
+                even_sum += n
+            else:
+                odd_sum += n
+
+        return odd_sum - even_sum
+
+
+# print(add(""))
+# print(add("1,2"))
+# print(add("1"))
+# print(add("1,2,3,4"))
+# print(add('1\n2,3'))
+# print(add('//;\n1;2'))
+# print(add('//[***]\n1***2***3'))
+# print(add('//[*][%]\n1*2%3'))
+# print(add('-1, -2, -3, 1, 2, 3'))
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,5 +1,7 @@
 
-from string_calculator import add
+from string_calculator import add, add_diff
+
+import pytest
 
 def test_add_one_number():
     assert add("1") == 1
@@ -14,17 +16,22 @@ def test_multiple_numbers():
     assert add("1,2,3,4") == 10
 
 def test_new_line_as_delimiter():
-    assert string_calculator.add('1\n2,3') == 6
+    assert add('1\n2,3') == 6
 
 def test_different_delimiters():
-    assert string_calculator.add('//;\n1;2') == 3
+    assert add('//;\n1;2') == 3
 
 def test_negative_numbers_exception():
     with pytest.raises(Exception, match = r'negatives not allowed \[-1, -2, -3\]'):
-        string_calculator.add('-1, -2, -3, 1, 2, 3')
+        add('-1, -2, -3, 1, 2, 3')
 
 def test_delimiters_any_length():
-    assert string_calculator.add('//[***]\n1***2***3') == 6
+    assert add('//[***]\n1***2***3') == 6
 
 def test_multiple_delimiters():
-    assert string_calculator.add('//[*][%]\n1*2%3') == 6
+    assert add('//[*][%]\n1*2%3') == 6
+
+#even = 1 + 3 = 4 odd - 2 + 4 = 6 diff -> odd - even = 2
+
+def test_diff_odd_and_even():
+    assert add_diff('1,2,3,4') == 2
